8893c55a1d0d5d72dc030ca448ce97bb0e0001cc
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2026-02-19 16:39:49 -0300

    feat: implement Dead Letter Queue (DLQ) for failed notification handling

--- src/main/java/com/artdev/notificationservice/config/RabbitMQConfig.java
+++ src/main/java/com/artdev/notificationservice/config/RabbitMQConfig.java
@@ -1,6 +1,6 @@
 package com.artdev.notificationservice.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -10,17 +10,34 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String QUEUE_NAME = "notification.queue";
+    public static final String DLQ_NAME = "notification.queue.dlq";
+    public static final String DLX_NAME = "notification.dlx";
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return QueueBuilder.durable(DLQ_NAME).build();
+    }
+
+    @Bean
+    public DirectExchange deadLetterExchange() {
+        return new DirectExchange(DLX_NAME);
+    }
+
+    @Bean
+    public Binding dlqBinding() {
+        return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange()).with(QUEUE_NAME);
+    }
 
     @Bean
     public Queue notificationQueue() {
-        // O 'TRUE' significa que essa fila é duravel, ou seja, ela sobreviverá a reinicializações do RabbitMQ
-        return new Queue(QUEUE_NAME, true);
+        return QueueBuilder.durable(QUEUE_NAME)
+                .withArgument("x-dead-letter-exchange", DLX_NAME) // Onde mandar se falhar
+                .withArgument("x-dead-letter-routing-key", QUEUE_NAME) // Com qual chave
+                .build();
     }
 
     @Bean
     public MessageConverter messageConverter() {
-        // Esse metodo é responsável por converter objetos Java em mensagens JSON e vice-versa,
-        // facilitando a comunicação com RabbitMQ
         return new Jackson2JsonMessageConverter();
     }
 }
