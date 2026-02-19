a54646e8de6d1c4135e631ef081c0805b56186b7
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2026-02-19 15:21:15 -0300

    feat: integrate JavaMailSender with Mailtrap for real email delivery

--- src/main/java/com/artdev/notificationservice/consumer/NotificationConsumer.java
+++ src/main/java/com/artdev/notificationservice/consumer/NotificationConsumer.java
@@ -5,6 +5,8 @@ import com.artdev.notificationservice.dto.NotificationRequestDTO;
 import com.artdev.notificationservice.service.TemplateService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,17 +15,26 @@ public class NotificationConsumer {
     @Autowired
     private TemplateService templateService;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
     public void consumeMessage(NotificationRequestDTO message){
 
         String templateContent = templateService.getTemplate(message.getTemplate());
 
-        if(templateContent == null){
-            System.out.println("Aviso: Template " + message.getTemplate() + " não encontrado no Redis!");
-            templateContent = message.getMessage();
+        String finalMessage = (templateContent != null) ? templateContent : message.getMessage();
+
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(message.getDestination());
+        email.setSubject("Nova Notificação do Sistema");
+        email.setText(finalMessage);
+
+        try {
+            mailSender.send(email);
+            System.out.println("E-mail enviado com sucesso para: " + message.getDestination());
+        } catch (Exception e) {
+            System.err.println("Erro ao enviar e-mail: " + e.getMessage());
         }
-        System.out.println("=== PROCESSANDO NOTIFICAÇÃO ===");
-        System.out.println("Usando Template: " + templateContent);
-        System.out.println("Enviando para: " + message.getDestination());
     }
 }
