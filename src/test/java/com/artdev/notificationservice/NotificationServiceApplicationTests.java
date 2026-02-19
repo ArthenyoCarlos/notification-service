d21bf78b6553df87108d13586d08e49791a24b37
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2026-02-19 13:31:56 -0300

    initialize notification service with Spring Boot and Maven wrapper

---
+++ src/test/java/com/artdev/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artdev.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
